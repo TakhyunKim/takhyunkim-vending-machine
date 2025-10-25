540b2298bd75ed4fc4ce5c819f54a4c3f42e7b78
TakhyunKim <kimkih1218@gmail.com>
2025-10-25 19:26:58 +0900

    docs: update card gateway js docs

--- src/entities/Payment/lib/CardGateway.ts
+++ src/entities/Payment/lib/CardGateway.ts
@@ -3,8 +3,10 @@ import type { CardPaymentInfo } from "../model";
 /**
  * @description 카드 게이트웨이 인터페이스
  *
+ * - 각 카드 사에 맞는 게이트웨이를 구현하여 사용하기 위한 추상 클래스
+ *
  * - 승인: 카드사에 결제 요청을 보내고 승인 번호를 받는 과정
- * - 캡처: 승인 번호를 사용하여 결제를 완료하는 과정
+ * - 구매: 승인 번호를 사용하여 결제를 완료하는 과정
  * - 취소: 결제를 취소하는 과정
  */
 export abstract class CardGateway {
